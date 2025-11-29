39088c1f98a8ba670e988eed536610105a5bc3aa
Maïmouna SARR <101868721+maichou12@users.noreply.github.com>
2025-11-29 22:20:57 +0000

    initial commit

---
+++ src/main/java/com/groupeisi/m2gl/trx_engine_g4/TrxEngineG4Application.java
@@ -0,0 +1,13 @@
+package com.groupeisi.m2gl.trx_engine_g4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrxEngineG4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrxEngineG4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/groupeisi/m2gl/trx_engine_g4/TrxEngineG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.m2gl.trx_engine_g4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrxEngineG4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
